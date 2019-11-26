47fec472be38c34e109a83439592133eea995454
Joe-Wayne Davis <joewaynejdavis@gmail.com>
2019-11-26 16:46:23 -0500

    about info input version 1

--- aplsemantics/lib/About.dart
+++ aplsemantics/lib/About.dart
@@ -1,9 +1,36 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 
 class AboutPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+
+  Widget build(BuildContext context) 
+  {
+      Widget titleSection = Container
+      (
+        
+                          child: Column
+                          (
+                            children: [
+                              Container 
+                              (
+                                child: Text 
+                                (
+                                  'Joe-Wayne Davis 1501061',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ) ,
+                                ),
+                            Text 
+                            (
+                              'University Of Technology Jamaica',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                            )
+                          ],
+                         )
+          
+      );
           return Scaffold(
             appBar : AppBar( 
               title: Text('Semantics App'),
